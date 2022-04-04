b1bc3994bf65c7f289acecea496a271d51639edb
McKay <wmsmckay@gmail.com>
2022-04-04 16:07:25 -0600

    testing font-awesome script

---
+++ js/font-awesome.js
@@ -0,0 +1,8 @@
+(function() {
+    var css = document.createElement('link');
+    // css.href = '//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css';
+    css.href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css';
+    css.rel = 'stylesheet';
+    css.type = 'text/css';
+    document.getElementsByTagName('head')[0].appendChild(css);
+})();
