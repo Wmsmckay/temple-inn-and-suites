77dcc845fc05d43a4a6de9d888bac3ec00ba2cfb
McKay <wmsmckay@gmail.com>
2022-04-04 16:41:50 -0600

    added responsive map to footer

---
+++ js/loadMap.js
@@ -0,0 +1,20 @@
+let map = document.querySelector('#mymap');
+
+maplistner = function (e) { 
+    let frame = document.createElement ('iframe'); 
+    frame.src = this.getAttribute ('data-src'); 
+    map.appendChild (frame); 
+    map.removeEventListener ("mouseover", maplistner); 
+}; 
+
+map.addEventListener ('mouseover', maplistner);
+
+window.addEventListener('load', 
+    function(e) {
+         setTimeout(function(){
+             let map = document.querySelector('#mymap'); 
+             let frame = document.createElement ('iframe'); 
+             frame.src = map.getAttribute ('data-src'); 
+             map.appendChild (frame); 
+    }, 1500);
+}); 
