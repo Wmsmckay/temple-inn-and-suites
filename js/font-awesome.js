96dff3a5bcaeb72d04defccd5b42a1b8f9a195eb
McKay <wmsmckay@gmail.com>
2022-04-04 17:30:46 -0600

    moved external stylesheets to js function

--- js/font-awesome.js
+++ js/font-awesome.js
@@ -6,3 +6,12 @@
     css.type = 'text/css';
     document.getElementsByTagName('head')[0].appendChild(css);
 })();
+
+(function() {
+    var css = document.createElement('link');
+    // css.href = '//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css';
+    css.href = 'https://fonts.googleapis.com/css2?family=Lato&display=swap';
+    css.rel = 'stylesheet';
+    css.type = 'text/css';
+    document.getElementsByTagName('head')[0].appendChild(css);
+})();
